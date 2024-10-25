a2cf31bda9de25c5190f8830aa1ff5fd8c6f824c
Ludmila Petrovna <LudmilaPetrovna777@hotmail.com>
2024-10-25 08:56:51 +0400

    sync

---
+++ RepackTools/gta-boot.sh
@@ -0,0 +1,5 @@
+mount //192.168.1.10/Gta-Micro gta-micro/ -o username=42
+git clone https://github.com/LudmilaPetrovna/gta-city-generator/
+mkdir cache
+cd cache
+find /dev/shm/gta-micro/Test/ -iname "*.img" | while read aa; do rr=`sed "s,/dev/shm/gta-micro/Test/,img_unpacked/,g;s,.img$,,g" <<< $aa`; echo "$aa -> $rr";curl -o tmp.img file://"$aa" tmp.img; perl /dev/shm/gta-city-generator/Lesson3/unpacker.pl tmp.img "$rr";rm tmp.img;done
