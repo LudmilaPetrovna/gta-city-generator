a2c459f668094ec4ff4bf40353545636f385feda
Ludmila Petrovna <LudmilaPetrovna777@hotmail.com>
2025-10-14 10:16:22 +0400

    sync

--- bytecode_decoder/commands_parse.pl
+++ bytecode_decoder/commands_parse.pl
@@ -18,6 +18,28 @@ CRunningScript::ReadParametersForNewlyStartedScript(this, started); (0x4Fu and 0
 
 @codes=map{"u"}(0..0x7fff);
 
+=pod
+Legend:
+i - input values, mostly ints or offsets
+o - output values
+p - input pointer to var (no immediate value)
+g - input pointer to global variable
+b - input byte (maybe flag or var)
+s - input string values, number=max string length
+v - vararg (enables 0 type)
+
+Stats:
+used_opcode_types (0..20)
+min_int
+max_int
+min_float
+max_float
+min_str_len
+max_str_len
+used_chars
+last_10_values
+=cut
+
 foreach $code(@cmds){
 if($code=~s/\n {8}+((case ([x\da-fA-F]+)u?: ?)+)//s){
 $idlist=$1;
@@ -26,44 +48,148 @@ $idlist=$1;
 
 while($idlist=~/case ([x\da-fA-F]+)u?:/g){
 $opcode=hex2dec($1);
+$exists[$opcode]=1;
+$snippets[$opcode]=$code;
 $params="";
 #print "Decoding opcode $opcode\n";
 
 foreach $line(@lines){
 #print "$line\n";
-if($line=~/CRunningScript::StoreParameters[^,]+,\s+([x\d]+)/){
-$params.="p".hex2dec($1); # fixme
+if($line=~/CRunningScript::StoreParameters[^,]+,\s+([x\da-fA-F]+)/){
+$params.="o".hex2dec($1); # fixme
 }
 if($line=~/CRunningScript::GetPointerToScriptVariable/){
 $params.="p1"; # fixme int 1 operand
 }
-if($line=~/CRunningScript::CollectParameters[^,]+,\s+([x\d]+)u?\)/){
-$params.="p".hex2dec($1);
+if($line=~/CRunningScript::GetIndexOfGlobalVariable/){
+$params.="g1"; # fixme int 1 operand
 }
-if($line=~/CRunningScript::ReadTextLabelFromScript[^,]+,[^,]+,\s+([x\d]+)u?\)/){
+if($line=~/CRunningScript::CollectParameters[^,]+,\s+([x\da-fA-F]+)u?\)/){
+$params.="i".hex2dec($1);
+}
+if($line=~/CRunningScript::ReadTextLabelFromScript[^,]+,[^,]+,\s+([x\da-fA-F]+)u?\)/){
 $params.="s".hex2dec($1);
 }
 if($line=~/CRunningScript::ReadParametersForNewlyStartedScript/){
-$params.="o";
+$params.="v";
 }
+
 if($line=~/CRunningScript::LocateCharCommand/){
 if($opcode<0xFE || $opcode>0x103){$params.="p6";} else {$params.="p8";}
 }
 
+if($line=~/CRunningScript::LocateCharCharCommand/){
+if($opcode<0x104 || $opcode>0x106){
+$params.="i5";
+} else {
+$params.="i6";
+}
+}
 
+if($line=~/CRunningScript::CharInAreaCheckCommand/){ # not sure, logic got from compiler
+if($opcode>=0x1a6){
+$params.="p8";
+} else {
+$params.="p6";
+}
+}
 
+if($line=~/CRunningScript::ScriptTaskPickUpObject/){
+$params.="i7s24s16i1";
+}
 
-if($line=~/sub_470150/){
-$params.="p1s24s16";
-if($opcode==0xA1A){$params.="p6";}
-if($opcode==0x88A){$params.="p8";}
-if($opcode==0x605){$params.="p6";}
-if($opcode==0x812){$params.="p6";}
+if($line=~/CRunningScript::ObjectInAreaCheckCommand/){
+if($opcode==1258){
+$params.="i8";
+} else {
+$params.="i6";
+}
 }
 
 
-if($line=~/CRunningScript::CharInAreaCheckCommand/){
-$params.="p6"; # fixme
+
+if($line=~/sub_487A20/){
+if($opcode<431 || $opcode>432){
+$params.="p6";
+} else {
+$params.="p8";
+}
+}
+
+if($line=~/sub_487720/){
+if($opcode>=0x0474){
+$params.="p6";
+} else {
+$params.="p5";
+}
+}
+
+
+
+if($line=~/sub_488EC0/){
+if($opcode==177 || $opcode==428){
+$params.="p8";
+} else {
+$params.="p6";
+}
+}
+
+
+if($line=~/CScriptThread::locateCar/){
+if($opcode<517 || $opcode>519){
+$params.="p5";
+} else {
+$params.="p6";
+}
+}
+
+if($line=~/return 0/){last;}
+
+
+
+
+if($line=~/sub_488780/){
+if($opcode==0x72E){
+$params.="i8";
+} else {
+$params.="i6";
+}
+}
+
+
+if($line=~/sub_487D10/){
+if($opcode==1254){
+$params.="i8";
+} else {
+$params.="i6";
+}
+}
+
+
+if($line=~/sub_4883F0/){
+if($opcode==2276){
+$params.="i9";
+} else {
+$params.="i7";
+}
+}
+
+
+if($line=~/sub_487F60/){
+if($opcode<1532 || $opcode>1537){
+$params.="i7";
+} else {
+$params.="i9";
+}
+}
+
+
+if($line=~/sub_470150/){
+$params.="i1s24s16";
+if($opcode==0xA1A){$params.="i6";}
+if($opcode==0x88A){$params.="i8";}
+if($opcode==0x605){$params.="i6";}
+if($opcode==0x812){$params.="i6";}
 }
 
 
@@ -75,6 +201,18 @@ if($opcode==0x1d || $opcode==0x1e){
 $params.="p1"; # GetPointerToScriptVariable
 }
 
+if($opcode==0x0180){
+$params="b1";
+}
+if($opcode==0x08DB){
+$params.="s8"x10;
+}
+
+
+if($opcode==0x06E6){
+$params="i1o1";
+}
+
 
 $codes[$opcode]=$params;
 }
@@ -83,6 +221,8 @@ $codes[$opcode]=$params;
 }
 
 write_file("opcode_db_my_operands.json",encode_json(\@codes));
+write_file("opcode_db_my_exists.json",encode_json(\@exists));
+write_file("opcode_db_my_snippets.json",encode_json(\@snippets));
 
 print $codes[0x087]."\n";
 
---
+++ bytecode_decoder/opcode_tool_lencompare.pl
@@ -0,0 +1,107 @@
+use JSON;
+use File::Slurp;
+
+$my_len=decode_json(read_file("opcode_db_my_operands.json"));
+for($q=0;$q<3000;$q++){
+$ops=$my_len->[$q];
+$count=0;
+while($ops=~/([a-z])(\d*)/g){
+($type,$num)=($1,$2*1);
+if($type eq "s"){$num=1;}
+if($type eq "v"){$num=1000;}
+$count+=$num;
+}
+
+$myl[$q]=$count;
+
+}
+
+
+$my_exists=decode_json(read_file("opcode_db_my_exists.json"));
+$my_code=decode_json(read_file("opcode_db_my_snippets.json"));
+
+$cleo_sa_cmds=decode_json(read_file("opcode_db_cleo.json"))->{extensions}->[0]->{commands};
+
+foreach $cmd(@{$cleo_sa_cmds}){
+$id=hex($cmd->{id});
+$cleo[$id]=$cmd->{num_params};
+$names[$id]=$cmd->{name};
+}
+
+open(dd,"opcode_db_ghidra_scm_len.txt");
+while(<dd>){
+if(/^([\da-f]{4}), (\d+)/i){
+$ghidra[hex($1)]=$2;
+}
+}
+close(dd);
+
+@sc_cmds=split(/<Command/s,read_file("opcode_db_gta3sc.xml"));
+foreach(@sc_cmds){
+$id=-1;
+if(/ID=\"0x([\da-f]+)/i){
+$id=hex($1);
+}
+if(/Name=\"([^\"]+)\"/){
+$names[$id]=$1;
+}
+$count=0;
+while(/<Arg /g){
+$count++;
+}
+if($id>=0){
+$sc[$id]=$count;
+}
+}
+
+
+
+open(dd,"opcode_db_wiki.wiki");
+while(<dd>){
+if(!/^\|\[\[/){next;}
+@cols=split(/\|\|/);
+$id=-1;
+$paramcount=-1;
+$icon=0;
+if($cols[0]=~/\[\[([\da-f]{4})\]\]/i){
+$id=hex($1);
+}
+
+if($cols[1]=~/\{\{Icon\|SA\}\}|\{\{Icon\|t\}\}/){
+$icon=1;
+}
+
+if($cols[2]=~/(\d+)/){
+$paramcount=$1;
+}
+
+if($icon){
+$wiki[$id]=$paramcount;
+}
+}
+ 
+
+#############
+
+for($q=0;$q<3000;$q++){
+if($my_exists->[$q]!=1){next;}
+$res="";
+if($cleo[$q]==$ghidra[$q] && $sc[$q]==$wiki[$q] && $ghidra[$q]==$sc[$q]){
+$res="other_ok";
+}
+if($sc[$q]==$myl[$q]){
+$res="all_ok";
+next;
+}
+if($my_code->[$q]=~/return/){
+print "\x1b[1;44;33m";
+printf("% 4s % 4s (% 50s) % 5s % 5s % 5s % 5s % 5s % 10s\n","hex","dec","name","cleo","ghidr","sc","wiki","my","res");
+printf("%04X %04d (% 50s) % 5d % 5d % 5d % 5d % 5d % 10s\n",$q,$q,$names[$q],$cleo[$q],$ghidra[$q],$sc[$q],$wiki[$q],$myl[$q],$res);
+print "\x1b[0m\x1b[92m".$my_code->[$q]."\x1b[0m\n";
+
+}
+
+}
+
+
+
