40154b8d024941522a7e73a018eb288a56c41e2b
Ludmila Petrovna <LudmilaPetrovna777@hotmail.com>
2026-03-16 22:59:20 +0400

    sync

--- IngameTools/CutsceneViewer/extract_cutscenes.pl
+++ IngameTools/CutsceneViewer/extract_cutscenes.pl
@@ -1,16 +1,52 @@
 use Data::Dumper;
+use File::Slurp;
+
+%cutdata=();
+%cutcode=();
+%cutorder=();
+@id2cut=();
+$cutcurid=0;
+
+$is_cj_only_char={};
+map{$is_cj_only_char->{$_}=1}qw/set_char_coordinates set_char_coordinates_dont_warp_gang set_char_coordinates_no_offset set_char_heading set_current_char_weapon/;
 
 @want=qw/load_cutscene switch_widescreen force_weather_now force_weather set_area_visible set_char_coordinates set_char_heading load_mission_text set_time_of_day set_time_of_day set_current_char_weapon set_extra_colours set_fading_colour load_scene clear_area/;
 #push(@want,qw/set_darkness_effect set_char_has_used_entry_exit/);
 
+$onclear="clear_cutscene|script_name";
+$onwant=join("|",sort @want);
+$is_multiple={};
+$is_multiple{clear_area}=1;
 
 
+# step 1: read cutscenes info from cuts.img (unpacked in 'cuts' dir)
+read_cuts_dir();
 
+# step 2: read decompiled main script
+parse_script();
 
+#add ids
+foreach $cutid(keys %cutdata){
+if(!exists $cutorder{$cutid}){print STDERR "Can't find order for $cutid scene! gen new $cutcurid\n";$cutorder{$cutid}=$cutcurid++;}
+if(!exists $cutcode{$cutid}){
+print STDERR "Can't find code initialization for $cutid scene! This scene may be very simple\n";
+$code="load_scene ".$cutdata{$cutid}->[1]."\n";
+} else {
+$code=$cutcode{$cutid};
+}
+$code=':prepare_'.($cutorder{$cutid}+1)."\n".$code;
+$code.="cutscene_name='".uc($cutid)."'\n";
+$code.="cutscene_dur=".$cutdata{$cutid}->[0]."\n";
+$code.="return\n\n";
+$cutcode{$cutid}=$code;
+print "order: $cutid $cutorder{$cutid}\n";
+$id2cut[$cutorder{$cutid}]=$cutid;
+}
 
+# step 3: produce jump table
 
 #switch gen
-$count=156;
+$count=$cutcurid;
 $maxswitch=64;
 $mswcount=int($count/64+.999);
 @ids=();
@@ -66,84 +102,22 @@ print "\n";
 }
 }
 
+for($q=0;$q<$cutcurid;$q++){
+$cutid=$id2cut[$q];
+print "// $q $cutid\n";
+print $cutcode{$cutid};
+}
+
 die;
 
 
 
 
 
-$is_cj_only_char={};
-map{$is_cj_only_char->{$_}=1}qw/set_char_coordinates set_char_coordinates_dont_warp_gang set_char_coordinates_no_offset set_char_heading set_current_char_weapon/;
-
-$onclear="clear_cutscene|script_name";
-$onwant=join("|",sort @want);
-
-$is_multiple={};
-$is_multiple{clear_area}=1;
-
-$current={};
-
-open(ii,"main[0].txt");
-while(<ii>){
-s/[\r\n]//sg;
-if(/^([\dA-F]{4}):\s+(\S+)(.*)/){
-($opcode,$command,$params)=($1,lc($2),$3);
-
-if($command eq 'start_cutscene'){
-print_scene();
-next;
-}
-
-if($command=~/$onclear/){
-print_scene();
-next;
-}
-
-if($command=~/$onwant/){
-if($command eq 'force_weather'){$command='force_weather_now';}
-if($is_cj_only_char->{$command} && index($params,' $3 ')!=0){next;}
-if(index($params,'@')>=0){next;}
-if($params=~/\$(\d+)/){
-if($1 != 3 ){
-#print "skipping variable $command$params\n";
-next;
-}
-}
-if($command eq 'set_current_char_weapon' && index($params,'WeaponType.Unarmed')<0){next;}
-
-if($command eq 'load_cutscene'){
-$params=~s/\s+\{name\}\s*//s;
-$current_name=$params;
-if($cut_uid>=134){
-print_scene();
-}
-next;
-}
 
 
-$current->{$command}=$params;
 
 
-}
-
-}
-}
-
-
-sub print_scene{
-
-if($current_name){
-print ":prepare_".(++$cut_uid)."\n";
-print map{"$_$current->{$_}\n"}keys %{$current};
-print "cutscene_name=$current_name;\n";
-print "return\n";
-print "\n";
-}
-
-$current={};
-$current_name="";
-
-}
 
 
 
@@ -175,3 +149,154 @@ clear:
 bad:
 00C0: set_time_of_day {hours} 359@ {minutes} 360@
 =cut
+
+
+sub read_cuts_dir{
+# read cutscenes info
+my @cuts=grep{/\.ifp$/i}read_dir("cuts");
+foreach $cutid(@cuts){
+$cutid=~s/\.ifp$//si;
+#print "Reading cutscene $cutid info...\n";
+
+$timemax=0;
+
+# read cut-file
+$cut_file='cuts/'.$cutid.'.cut';
+$in_text=0;
+$in_info=0;
+foreach(read_file($cut_file)){
+s/[\r\n]//sg;
+
+if(/^end$/){
+$in_text=0;
+$in_info=0;
+next;
+}
+
+if(/^info$/){
+$in_info=1;
+next;
+}
+
+if(/^text$/){
+$in_text=1;
+next;
+}
+
+if($in_info){
+@p=split(/\s+/);
+if($p[0] eq "offset"){$offset=join(" ",@p[1..3]);}
+
+}
+
+if($in_text){
+($timestart,$timelen,$text)=split(/\s*,\s*/,$_,3);
+$timeend=$timestart+$timelen;
+if($timemax<$timeend){$timemax=$timeend;}
+}
+
+
+}
+
+
+#read dat-files
+foreach(read_file('cuts/'.$cutid.'.dat')){
+s/[\r\n]//sg;
+@p=split(/\s*,\s*/);
+
+if(@p>3 && $p[0]=~/^([\d\.\-]+)/){
+$timecode=int($1*1000);
+if($timemax<$timecode){$timemax=$timecode;}
+}
+
+
+}
+
+$cutdata{$cutid}=[$timemax,$offset];
+#print "max time: $timemax\n";
+
+
+}
+
+#print Dumper(\%cutdata);
+}
+
+
+sub parse_script{
+
+
+$current={};
+
+open(ii,"main[0].txt");
+while(<ii>){
+s/[\r\n]//sg;
+if(/^([\dA-F]{4}):\s+(\S+)(.*)/){
+($opcode,$command,$params)=($1,lc($2),$3);
+
+if($command eq 'start_cutscene'){
+gen_scene_code();
+next;
+}
+
+if($command=~/$onclear/){
+gen_scene_code();
+next;
+}
+
+if($command=~/$onwant/){
+if($command eq 'force_weather'){$command='force_weather_now';}
+if($is_cj_only_char->{$command} && index($params,' $3 ')!=0){next;}
+if(index($params,'@')>=0){next;}
+if($params=~/\$(\d+)/){
+if($1 != 3 ){
+#print "skipping variable $command$params\n";
+next;
+}
+}
+if($command eq 'set_current_char_weapon' && index($params,'WeaponType.Unarmed')<0){next;}
+
+if($command eq 'load_cutscene'){
+$params=~s/\s+\{name\}\s*//s;
+$current_name=$params;
+$cutid=lc($current_name);
+$cutid=~s/[\"\']+//sg;
+if(!exists $cutorder{$cutid}){
+$cutorder{$cutid}=$cutcurid++;
+#print "DEBUG: $cutid $cutorder{$cutid}\n";
+}
+next;
+}
+
+
+$current->{$command}=$params;
+
+
+}
+
+}
+}
+
+
+}
+
+sub gen_scene_code{
+my $code="";
+if($current_name){
+
+if(!exists $cutdata{$cutid}){die "can't find data entry for $cutid $cutdata{$cutid}";}
+
+if($current->{set_area_visible}){ #always be first
+$code='set_area_visible'.$current->{set_area_visible}."\n";
+delete $current->{set_area_visible};
+}
+
+if(!exists $current->{load_scene} && !exists $current->{load_scene_in_direction}){
+$current->{load_scene}=' '.$cutdata{$cutid}->[1];
+}
+$code.=join("",map{"$_$current->{$_}\n"}sort keys %{$current});
+$cutcode{$cutid}=$code;
+}
+$current={};
+$current_name="";
+}
+
---
+++ IngameTools/CutsceneViewer/unpack_cuts.sh
@@ -0,0 +1,1 @@
+perl /dev/shm/gta-city-generator/Lesson3/unpacker.pl /dev/shm/t/gta/Grand\ Theft\ Auto\ -\ San\ Andreas/anim/cuts.img cuts
