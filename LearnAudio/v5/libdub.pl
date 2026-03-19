55a9845c158557fcfca874b00d0f2d4065a45d39
Ludmila Petrovna <LudmilaPetrovna777@hotmail.com>
2026-03-19 20:33:09 +0400

    sync

--- LearnAudio/v5/libdub.pl
+++ LearnAudio/v5/libdub.pl
@@ -4,11 +4,14 @@ use JSON;
 use Encode;
 use File::Slurp;
 use Data::Dumper;
+use File::Path qw(make_path remove_tree);
+use File::Basename;
+use Digest::CRC qw(crc64 crc32 crc16 crcccitt crc crc8 crcopenpgparmor);
 
 binmode(STDOUT,":utf8");
 
 my $src_lang="es";
-
+my $flac_repo="flac_repo";
 
 
 my($status,$debug,$join)=calc_status('atlas_spa_0');
@@ -24,13 +27,17 @@ my($st,$en,$eng,$rus)=@{$join->{$_}};
 push(@{$cuts},[$_,$st,$en]);
 }
 my $buf;
+my($package_name,$bank_id,$sound_id);
 foreach(sort{$a->[1] <=> $b->[1]}@{$cuts}){
 my($srcfile,$st,$en)=@{$_};
-my $outname=$srcfile;
-$outname=~s/\//_/gs;
-$outname=~s/\.wav$/.flac/s;
 
-print "Cutting $srcfile from $st .. $en --> $outname\n";
+if($srcfile=~/sound_sfx\/([^\/]+)\/bank(\d+)\/sound_(\d+)\.wav/){
+($package_name,$bank_id,$sound_id)=($1,$2|0,$3|0);
+
+} else {die "wrong format";}
+
+
+
 print_join_entry($srcfile);
 my $skip=int($st*48000)-$cursamplepos;
 if($skip<0){die "Can't rewwind $skip!";}
@@ -45,12 +52,12 @@ print "we now at $cursamplepos\n";
 $len*=2;
 if(read(ii,$buf,$len)!=$len){die "Can't read $len usable bytes!";}
 
-my $pcmname=$outname;
-$pcmname=~s/\.flac/.pcm/s;
+my $flacname="tmp-cutvoice-${package_name}_${bank_id}_${sound_id}.flac";
+my $pcmname="tmp-cutvoice-${package_name}_${bank_id}_${sound_id}.pcm";
+my $pcmname2="tmp-cutvoice-${package_name}_${bank_id}_${sound_id}.wav";
+
 write_file($pcmname,$buf);
 
-my $pcmname2=$outname;
-$pcmname2=~s/\.flac/.wav/s;
 open(oo,"|ffmpeg -v 0 -f s16le -ar 48000 -ac 1 -i - -y $pcmname2");
 print oo $buf;
 close(oo);
@@ -58,6 +65,12 @@ close(oo);
 
 my($total_samples,$leading_zeros,$trailing_zeros,$first_nonzero,$useful_length,$silence_flag)=split(/\s/s,`./calc_duration "$pcmname"`);
 print "anal ($total_samples,$leading_zeros,$trailing_zeros,$first_nonzero,$useful_length,$silence_flag)\n";
+
+unlink($pcmname);
+unlink($pcmname2);
+
+
+
 if(($leading_zeros<50 && !$is_first) || $trailing_zeros<50){
 print STDERR "$pcmname: File may be trimmed!";
 next;
@@ -73,13 +86,15 @@ print STDERR "$pcmname: File too short!";
 next;
 }
 
-unlink($pcmname);
-unlink($pcmname2);
-
 print "opening out\n";
-open(oo,"|ffmpeg -v 0 -f s16le -ar 48000 -ac 1 -i - -y $outname");
+open(oo,"|ffmpeg -v 0 -f s16le -ar 48000 -ac 1 -i - -y '$flacname'");
 print oo substr($buf,$leading_zeros*2,$useful_length*2);
 close(oo);
+
+my $flac_encoded=read_file($flacname);
+my $outname=sprintf("%s/raw_trans_voice/%s/bank_%04d/b%04d_s%04d_c%08x.flac",$flac_repo,lc($package_name),$bank_id,$bank_id,$sound_id,crc32($flac_encoded));
+make_path(dirname($outname));
+`mv "$flacname" "$outname"`;
 
 $is_first=0;
 
